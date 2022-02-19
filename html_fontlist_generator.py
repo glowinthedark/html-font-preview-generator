28a0e6e9810c1cecbf8546fd9ab06412ea56ad47
glowinthedark <48893368+glowinthedark@users.noreply.github.com>
2022-02-19 17:51:52 +0100

    sort by name

--- html_fontlist_generator.py
+++ html_fontlist_generator.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-# DEPENDENCIES: python3 and matplotlib; install with:
-#     pip3 install matplotlib
-
 from matplotlib import font_manager
 
 SAMPLES = {
@@ -22,6 +19,7 @@ if __name__ == "__main__":
     ttf_dict.update(afm_dict)
 
     with open(OUTPUT_FILE, mode="w", encoding="utf8") as out:
+
         print('''
     <!DOCTYPE html>
     <html>
@@ -57,6 +55,22 @@ if __name__ == "__main__":
                 text-shadow: rgb(51 49 42 / 40%) 1px 0px 5px;
             }
     
+            h3 {
+                color: #ccc;
+                text-align: center;
+                padding: 0.5em 20%;
+                font-size: 26pt;
+                margin: 0.5em 2em;
+            }
+    
+            h4 {
+                font-size: 18pt;
+                color: rgb(189, 189, 189);
+                margin: 0;
+                padding: 0;
+                text-align: center;
+            }
+    
             body,
             body>div.center.rounded {
                 min-height: 96%;
@@ -75,16 +89,92 @@ if __name__ == "__main__":
             hr {
                 border: 1px dotted #ccc;
             }
-        
+    
+            .caption {
+                font-size: 11pt;
+                color: orange;
+                text-align: center;
+                margin: 0;
+                padding: 0;
+            }
+    
             .flex_item {
                 flex: 0 0 300px;
             }
-
+            ::-webkit-input-placeholder {
+                /* WebKit, Blink, Edge */
+                letter-spacing: 0.1em;
+                color: lightgray;
+                font-size: 9pt;
+            }
+    
+            :-moz-placeholder {
+                /* Mozilla Firefox 4 to 18 */
+                letter-spacing: 0.1em;
+                color: lightgray;
+                opacity: 1;
+                font-size: 9pt;
+            }
+    
+            ::-moz-placeholder {
+                /* Mozilla Firefox 19+ */
+                letter-spacing: 0.1em;
+                color: lightgray;
+                opacity: 1;
+                font-size: 9pt;
+            }
+    
+            :-ms-input-placeholder {
+                /* Internet Explorer 10-11 */
+                letter-spacing: 0.1em;
+                color: lightgray;
+                font-size: 9pt;
+            }
+    
+            ::-ms-input-placeholder {
+                /* Microsoft Edge */
+                letter-spacing: 0.1em;
+                color: lightgray;
+                font-size: 9pt;
+            }
+    
+            ::placeholder {
+                /* Most modern browsers support this now. */
+                letter-spacing: 0.1em;
+                color: lightgray;
+                font-size: 9pt;
+            }
+    
+            .rounded {
+                -webkit-box-shadow: rgb(29 36 23 / 39%) -2px 4px 17px 0px;
+                -moz-box-shadow: rgba(29, 36, 23, 0.386719) -2px 4px 17px 0px;
+                box-shadow: rgb(29 36 23 / 39%) -2px 4px 17px 0px;
+                padding: 20px;
+                -webkit-border-radius: 11px;
+                -moz-border-radius: 11px;
+                border-radius: 11px;
+            }
+    
+            .fname {
+                font-size: 6pt;
+                font-family: 'JetBrains Mono', monospace;
+                color: gray;
+                border-top: 1px dotted gray;
+                padding-top: 2em;
+            }
     
             @media (max-width: 600px) {
-
+                * {
+                    font-size: 1.06rem;
+                }
+    
+                h1 a {
+                    margin: 0;
+                }
+    
                 .center {
                     display: flex;
+                    /* height: 98vh; */
                     justify-content: center;
                     align-items: center;
                 }
@@ -98,6 +188,17 @@ if __name__ == "__main__":
                     margin: 0.4em;
                 }
     
+                .overview {
+                    line-height: 150%;
+                    margin: 0;
+                }
+    
+                .msg {
+                    text-align: center;
+                    margin: 1em 0.4em;
+                    padding-bottom: 20px;
+                }
+    
                 h1 {
                     text-align: center;
                     box-sizing: border-box;
@@ -107,6 +208,28 @@ if __name__ == "__main__":
                     line-height: 140%;
                     color: gray;
                     font-size: 25pt;
+                }
+    
+                h3 {
+                    color: #ccc;
+                    text-align: center;
+                    padding: 0.5em 10%;
+                    font-size: 18pt;
+                    margin: 0.5em;
+                }
+    
+                #feedback {
+                    display: none;
+                    text-align: center;
+                    margin: 0;
+                    min-width: 100%;
+                    width: 100%;
+                }
+    
+                #feedback input, #feedback textarea {
+                    display: block;
+                    width: 100%;
+                    min-width: 100%;
                 }
     
                 .rounded {
@@ -127,7 +250,8 @@ if __name__ == "__main__":
         <ul>
     ''', file=out)
 
-        for family, font in afm_dict.items():
+        for family in sorted(ttf_dict.keys()):
+            font = ttf_dict[family]
             print(f'''
     <li>
         <p class="fname">{font.fname}</p>
